2deb53e6685910d0dca5ac2f27d82fbdbea35b6a
Jebb Burditt <jebb.burditt@gmail.com>
2024-04-11 13:06:16 -0700

    add throttle test to simulation

--- test/src/test/scala/simulation/UserSimulation.scala
+++ test/src/test/scala/simulation/UserSimulation.scala
@@ -8,14 +8,14 @@ class UserSimulation extends Simulation {
     setUp(
         //UserScenarios.createUserScenario.inject(rampUsers(250) during (15 minutes)),
         //UserScenarios.getAccessToken.inject(atOnceUsers(100))
-        //UserScenarios.getDriverInfo.inject(atOnceUsers(1)),
-        //UserScenarios.getMostRecentCase.inject(atOnceUsers(1)),
-        //UserScenarios.getClosedCases.inject(atOnceUsers(1)),
-        UserScenarios.getCaseDocuments.inject(atOnceUsers(1)),
+        UserScenarios.getDriverInfo.inject(atOnceUsers(1)),
+        UserScenarios.getMostRecentCase.inject(atOnceUsers(1)),
+        UserScenarios.getClosedCases.inject(atOnceUsers(1)),
+        UserScenarios.getCaseDocuments.inject(atOnceUsers(10)),
         UserScenarios.getAllDocuments.inject(atOnceUsers(1)),
-        //UserScenarios.getDocumentTypes.inject(atOnceUsers(1))
-    )
+        UserScenarios.getDocumentTypes.inject(atOnceUsers(1)),
 
-    // setUp(UserScenarios.getMostRecentCase.inject(constantUsersPerSec(40) during (2.minutes)))
-    //     .throttle(jumpToRps(40), holdFor(2.minutes))
+        UserScenarios.getMostRecentCase.inject(constantUsersPerSec(1) during (10.seconds))
+            .throttle(jumpToRps(2), holdFor(10.seconds))
+    )
 }
